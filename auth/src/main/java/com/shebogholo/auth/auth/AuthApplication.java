3f6812e641ae7c430dea5e4f77e6ae158f707b75
Shebogholo <shebogholo@gmail.com>
2022-10-20 20:48:57 +0300

    Added auth index endpoint

--- auth/src/main/java/com/shebogholo/auth/auth/AuthApplication.java
+++ auth/src/main/java/com/shebogholo/auth/auth/AuthApplication.java
@@ -1,13 +1,32 @@
 package com.shebogholo.auth.auth;
 
+import lombok.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public Message index() {
+        return Message.builder().message("Auth API is up and running on a Docker!").build();
+    }
+
+    @Builder
+    @Setter
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class Message {
+        private String message;
     }
 
 }
